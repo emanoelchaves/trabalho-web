a6a79cb2d400ce6ef02ea79677a62abd710d7945
Emanoel Chaves <emanoelchaves@alu.ufc.br>
2019-05-21 21:12:09 -0300

    Parte 2 do Trabalho
    Esta é a parte 2 do trabalho, foi realizada algumas mudanças na pagina de cadastro para se adequar ao banco de dados

---
+++ rest_persistence/src/main/java/rest/model/User.java
@@ -0,0 +1,67 @@
+package rest.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User {
+	
+	private int id;
+	private String username;
+	private String password;
+	private String name;
+	
+	public User() {
+		
+	}
+		
+	public User(int id, String username, String password, String name, int phone) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ",name=" + name;
+	}	
+}
